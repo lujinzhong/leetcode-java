b80eedecac5c712c2831f160b737176aad106868
MClink <602060558@qq.com>
2020-07-07 00:37:44 +0800

    有效的字母异位词-数组抵消法

--- src/leetcode242/Test.java
+++ src/leetcode242/Test.java
@@ -8,7 +8,13 @@ public class Test {
 
     }
 
-    public boolean isAnagram(String s, String t) {
+    /**
+     * 哈希表实现
+     * @param s 字符串1
+     * @param t 字符串 2
+     * @return true false
+     */
+    public boolean isAnagram1(String s, String t) {
         // 都是空字符串，通过
         if (s.equals("") && t.equals("")) return true;
         // 长度不同直接不通过
@@ -28,5 +34,33 @@ public class Test {
             }
         }
         return map1.equals(map2);
+    }
+
+    /**
+     * 数组-抵消计数法
+     * @param s 字符串s
+     * @param t 字符串 t
+     * @return true false
+     */
+    public boolean isAnagram2(String s, String t) {
+        // 都是空字符串，通过
+        if (s.equals("") && t.equals("")) return true;
+        // 长度不同直接不通过
+        if (s.length() != t.length()) return false;
+        int[] count = new int[26]; // 26 个字母
+        // 思路是，根据 26 字母的 ascll 值来求差索引
+        // 如果某个字母两个字符拥有的个数相同，则累加值可以相互抵消 变为 0
+        for (char c: s.toCharArray()
+             ) {
+            count[c - 'a'] +=1;
+        }
+        for (char c: t.toCharArray()
+        ) {
+            count[c - 'a'] -=1; // 相同的字母会变为0
+        }
+        for (int i=0 ;i < 26; i++) {
+            if(count[i] != 0) return false;
+        }
+        return true;
     }
 }
