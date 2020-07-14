327ec8c89a8f15e97332343b4b04730f2e4076ae
MClink <602060558@qq.com>
2020-07-15 00:59:00 +0800

    暴力破解和递归解-是不是有效的二叉搜索树

---
+++ src/utils/TreeNode.java
@@ -0,0 +1,14 @@
+package utils;
+
+public class TreeNode {
+    // 值
+    int val;
+    // 左孩子
+    TreeNode left;
+    // 右孩子
+    TreeNode right;
+    // 构造函数
+    TreeNode(int x) {
+        val = x;
+    }
+}
